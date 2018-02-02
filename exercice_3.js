e0298138809adbed892c794e67ce3bd283d1332a
Linna Lim <e1479687@cmaisonneuve.qc.ca>
2018-02-02 15:35:41 -0500

    connection avce mon JSON et mon let provinces

--- exercice_3.js
+++ exercice_3.js
@@ -1,9 +1,18 @@
 const http = require('http');
+const fs = require('fs');
+let provinces;
+
+fs.readFile('provinces.json', 'utf8', function (err, data) {
+	if (err) throw err;
+	provinces = JSON.parse(data);
+});
+
 http.createServer((request, response) =>{
 
-response.writeHead(200, {"Content-Type" : "text/html; charset=UTF-8"});
-response.write('<style>h1 {font-family:arial; font-size: 3em;}</style>');
-response.write('<h1> ça marche et vive le Québec</h1>');
-response.end();
+	console.log(provinces);
+	response.writeHead(200, {"Content-Type" : "text/html; charset=UTF-8"});
+	response.write('<style>h1 {font-family:arial; font-size: 3em;}</style>');
+	response.write('<h1> ça marche et vive le Québec</h1>');
+	response.end();
 
 }).listen(3000);
